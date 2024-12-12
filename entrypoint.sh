f6f6db58a0b97948d840bb1955c3aa5a5579be63
Jatin <jatin.kshatriya2821@gmail.com>
2024-12-12 16:53:00 +0530

    push db

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,21 +7,5 @@ python manage.py migrate --no-input
 # Collect static files
 python manage.py collectstatic --no-input --clear
 
-# Create superuser if it doesn't exist
-python manage.py shell <<EOF
-from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
-
-# Set superuser credentials, either from environment variables or hardcoded
-superuser_username = 'admin'
-superuser_email = 'admin@example.com'
-superuser_password = '1234'  # You can replace this with an env variable for security
-
-try:
-    User.objects.get(username=superuser_username)
-except ObjectDoesNotExist:
-    User.objects.create_superuser(superuser_username, superuser_email, superuser_password)
-EOF
-
 # Start server
 python manage.py runserver 0.0.0.0:10000
