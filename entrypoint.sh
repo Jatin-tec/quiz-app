81385bc9f19cc4405857d79765bc07cc87322d26
Jatin <jatin.kshatriya2821@gmail.com>
2024-12-12 16:47:11 +0530

    entrypoint fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Apply database migrations
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
@@ -5,8 +7,21 @@ python manage.py migrate --no-input
 # Collect static files
 python manage.py collectstatic --no-input --clear
 
-# Create superuser
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '1234')" | python manage.py shell
+# Create superuser if it doesn't exist
+python manage.py shell <<EOF
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+
+# Set superuser credentials, either from environment variables or hardcoded
+superuser_username = 'admin'
+superuser_email = 'admin@example.com'
+superuser_password = '1234'  # You can replace this with an env variable for security
+
+try:
+    User.objects.get(username=superuser_username)
+except ObjectDoesNotExist:
+    User.objects.create_superuser(superuser_username, superuser_email, superuser_password)
+EOF
 
 # Start server
 python manage.py runserver 0.0.0.0:10000
